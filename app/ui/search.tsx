272d96da7f61594f3477c9e7eae157ab2c5f0aed
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-31 00:53:21 +0600

    added debounce

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,15 +1,20 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+   
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -17,7 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
