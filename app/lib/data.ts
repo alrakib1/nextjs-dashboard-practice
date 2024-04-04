ce888eebe574b6b4ca0f756e581fc332b91e0120
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-04-04 23:48:28 +0600

    added error.tsx file

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -166,8 +166,7 @@ export async function fetchInvoiceById(id: string) {
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
-    }));
-
+    })); 
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
